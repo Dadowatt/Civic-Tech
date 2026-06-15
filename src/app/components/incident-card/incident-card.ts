060cb4e8daca2c8f8f82f88b111160db3b8cc6df
binetouguey3s <binetouguey3s@gmail.com>
2026-06-15 12:31:56 +0000

    architecture du projet

---
+++ src/app/components/incident-card/incident-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-card',
+  imports: [],
+  templateUrl: './incident-card.html',
+  styleUrl: './incident-card.css',
+})
+export class IncidentCard {}
