95a9d39ae44a453b8891e2202b52356d41b3fe08
Dadowatt <dadowatt92@gmail.com>
2026-06-16 01:40:58 +0200

    backup recovery-ui state

--- src/app/pages/incident-list/incident-list.ts
+++ src/app/pages/incident-list/incident-list.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IncidentInterface } from '../../models/incident.interface';
+import { IncidentService } from '../../services/incident.service';
 
 @Component({
   selector: 'app-incident-list',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './incident-list.html',
   styleUrl: './incident-list.css',
 })
-export class IncidentList {}
+export class IncidentList {
+  incidents: IncidentInterface[] = [];
+
+  constructor(private incidentService: IncidentService){}
+
+  ngOninit(){
+    this.incidentService.getIncidents().subscribe(data => {
+      this.incidents = data;
+    })
+  }
+}
