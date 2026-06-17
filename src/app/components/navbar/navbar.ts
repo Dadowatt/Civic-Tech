9d6c4984a2d00619109802b6f5d58e67ff873579
binetouguey3s <binetouguey3s@gmail.com>
2026-06-17 23:34:06 +0000

    rectifier

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-
+import { IncidentInterface } from '../../models/incident.interface';
+import { IncidentService } from '../../services/incident.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,20 @@ import { RouterLink } from "@angular/router";
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+ // nombe total de soutiens depuis IncidentList
+export class Navbar {
+  incidents: IncidentInterface[] = [];
+  support = 0;
+  constructor(private incidentService: IncidentService){}
+  ngOnInit(): void {
+    this.incidentService.getIncidents().subscribe(data => {
+      this.incidents = data;
+      this.support = this.incidents.reduce((acc, cur) => acc + cur.supports, 0);
+    });
+  }
+   get totalSupports(): number {
+    return this.incidents.reduce((acc, cur) => acc + cur.supports, 0);
+  } 
+
+}
+
